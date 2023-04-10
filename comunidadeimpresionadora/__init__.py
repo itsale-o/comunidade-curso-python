c54b6b8f10ba4e982edf1fc4e105a3338b486508
Alessandra Oliveira <106682325+itsale-o@users.noreply.github.com>
2023-04-10 11:05:39 -0300

    Update __init__.py

--- comunidadeimpresionadora/__init__.py
+++ comunidadeimpresionadora/__init__.py
@@ -8,6 +8,8 @@ import sqlalchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'e18e9c53ca9294cea12aae49b35cbed3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:MurZLbTbMVJk8avlRpFG@containers-us-west-68.railway.app:6644/railway'
+
 if os.getenv("DATABASE_URL"):
   app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
@@ -21,5 +23,4 @@ login_manager.login_message_category = 'alert-info'
 
 from comunidadeimpresionadora import models
 from comunidadeimpresionadora import routes
-
 
