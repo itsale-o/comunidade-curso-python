c78d730a3678898c5d5650545ed8a8d74f52cd08
Alessandra Oliveira <106682325+itsale-o@users.noreply.github.com>
2023-04-10 10:57:20 -0300

    Update __init__.py

--- comunidadeimpresionadora/__init__.py
+++ comunidadeimpresionadora/__init__.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'e18e9c53ca9294cea12aae49b35cbed3'
 if os.getenv("DATABASE_URL"):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -20,16 +20,6 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
 from comunidadeimpresionadora import models
-engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-inspection = sqlalchemy.inspect(engine)
-if not inspection.has_table('usuario', schema="dbo"):
-    with app.app_context():
-        database.drop_all()
-        database.create_all()
-        print('Base de dados criada com sucesso')
-else:
-    print('Base de dados já existente')
-
-
 from comunidadeimpresionadora import routes
+
 
